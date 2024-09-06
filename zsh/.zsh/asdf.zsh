5897b13d60935ab85674208da296bb918d6472b4
samm81 <samwmaynard@gmail.com>
2024-09-06 06:26:33 -0700

    vim: set `ASDF_NODEJS_VERSION`

--- zsh/.zsh/asdf.zsh
+++ zsh/.zsh/asdf.zsh
@@ -1,1 +1,2 @@
-alias vim='ASDF_NODEJS_VERSION=19.6.1 vim'
+ASDF_NODEJS_GLOBAL_VERSION=$(grep 'nodejs' "${HOME}/.tool-versions" | cut -d ' ' -f 2)
+alias vim='ASDF_NODEJS_VERSION="$ASDF_NODEJS_GLOBAL_VERSION" vim'
